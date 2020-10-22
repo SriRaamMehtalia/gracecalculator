c700b4b9e8fff004d6ebc1056c2bc419fb3f4fde
SriRaam Mehtalia <sriraammehtalia@SriRaams-MBP.hsd1.ca.comcast.net>
2020-10-21 20:10:08 -0700

    Updates

---
+++ precache-manifest.3b5327c9e91844439552b48c006ecb45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1e7b58af86c9fc98e0b9d4850f8cde6",
+    "url": "/gracecalculator/index.html"
+  },
+  {
+    "revision": "331ecf8a5d43edb5b5e7",
+    "url": "/gracecalculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5989a78f89a93f56edd2",
+    "url": "/gracecalculator/static/js/2.dc766714.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/gracecalculator/static/js/2.dc766714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "331ecf8a5d43edb5b5e7",
+    "url": "/gracecalculator/static/js/main.0ab9c53e.chunk.js"
+  },
+  {
+    "revision": "bd2282b1e3d076444a01",
+    "url": "/gracecalculator/static/js/runtime-main.f98c5dc9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gracecalculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
